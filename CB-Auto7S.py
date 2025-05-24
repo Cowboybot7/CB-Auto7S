ef20e1eb5b6e7e569c8c08fb46c1d9b4ef538df2
Cowboybot7 <cowboybot7@gmail.com>
2025-05-24 19:18:01 +0700

    Update CB-Auto7S.py

--- CB-Auto7S.py
+++ CB-Auto7S.py
@@ -1,4 +1,4 @@
-afrom selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
