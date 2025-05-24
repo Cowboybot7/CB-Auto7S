54bfe4b8743455cf5e8cd4284db5c8bbc9e4ef4d
Cowboybot7 <cowboybot7@gmail.com>
2025-05-24 17:08:35 +0700

    Update CB-Auto7S.py
    add healthz

--- CB-Auto7S.py
+++ CB-Auto7S.py
@@ -22,6 +22,8 @@ from math import radians, sin, cos, sqrt, atan2
 from selenium.common.exceptions import TimeoutException
 from threading import Lock
 from asyncio import create_task
+import asyncio
+from aiohttp import web
 
 def calculate_distance(lat1, lon1, lat2, lon2):
     """
@@ -474,10 +476,11 @@ async def letgo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     task = create_task(task_wrapper())
     scan_tasks[chat_id] = task
 
-def main():
+async def handle_health_check(request):
+    return web.Response(text="OK")
+    
+async def main():
     """Start the bot"""
-    # health_thread = threading.Thread(target=run_health_server, daemon=True)
-    # health_thread.start()
 
     application = Application.builder().token(TELEGRAM_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
@@ -485,16 +488,24 @@ def main():
     application.add_handler(CommandHandler("cancelauto", cancelauto))
     application.add_handler(CommandHandler("cancel", cancel))
     application.post_init = post_init
+
+    health_app = web.Application()
+    health_app.router.add_get('/healthz', handle_health_check)
+    
     WEBHOOK_URL = os.getenv('WEBHOOK_URL')
     PORT = int(os.getenv('PORT', 8000))
-
-    application.run_webhook(
+    
+    await application.initialize()
+    
+    await application.start_webhook(
         listen="0.0.0.0",
         port=PORT,
+        webhook_path="",
         webhook_url=WEBHOOK_URL,
-        # health_check_path='/healthz',
+        server=health_app,
         allowed_updates=Update.ALL_TYPES,
     )
-
+    await application.updater.wait_closed()
+    
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
